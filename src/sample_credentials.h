e9e45352198b0e2f0021944f4cea48fce9fbc6e5
Greg Baker <gregBaker.email@gmail.com>
2021-04-19 13:05:02 -0400

    Updates

--- src/sample_credentials.h
+++ src/sample_credentials.h
@@ -1,2 +1,3 @@
+// Add your ssid and password; change the file name to credentials.h; and put that file into your project's include directory.
 #define WIFI_SSID " ** your ssid ** "
 #define WIFI_PASS " ** your password ** "
